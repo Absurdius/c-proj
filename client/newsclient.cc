5ffe4671f7ecec98c13e11fcd91d0c329a4f9170
Absurdius <e94doclrd@hotmail.com>
2017-04-10 15:56:30 +0200

    added skeleton for console client

---
+++ client/newsclient.cc
@@ -0,0 +1,42 @@
+// client skeleton
+// infinite feeding loop
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void help()
+{
+  cout << "Available commands: \n";
+  cout << "quit ,quits the program \n";
+  cout << "help ,prints help \n";
+  cout << "more commands coming up. :) \n";
+}
+
+int main()
+{
+  string command;
+
+  cout << "Welcome to the newsgroup client. \n";
+  cout << "Write help for help \n";
+
+  do  {
+    cout << "What do you want to do? \n";
+    cin >> command;
+    cout << "You wrote: " << command << "\n";
+    if(command == "quit"){break;}
+    if(command == "help"){help();}
+
+    /*
+    switch (command)
+    {
+      case "quit": return 0;
+      case "help": help();
+      default : cout << "Not a command, write help for help.\n";
+    }
+    */
+  } while (cin);
+
+  return 0;
+}
