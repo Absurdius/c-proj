e942bc4b2eac890b9686d4a6f5872f49406716da
Absurdius <e94doclrd@hotmail.com>
2017-04-12 18:44:12 +0200

    pesudo implementerade klasser

--- server/article.cc
+++ server/article.cc
@@ -2,16 +2,33 @@
 The article class
 -----------------------*/
 #include<string>
-#include<random>
+#include<sstream>
+#include<cstdlib> //rand();
 
 class Article
 {
 public:
-  Article();
-  void print();
+  Article(string title, string author, string text);
+  string toString();
 private:
   string author;
   string title;
   string text;
   unsigned int id;
 };
+
+Article::Article(string t, string a, string txt)
+{
+	title = t; 
+	author = a; 
+	txt = txt;
+	// create random number
+	id = rand() % 10000; 
+}
+
+string Article::toString()
+{
+	stringstream ss; 
+	ss << title << "\n" << "by " << author << "\n" << "Article ID: " << id << "\n"; 
+	return ss.tostring();
+}
--- server/newsgroup.cc
+++ server/newsgroup.cc
@@ -5,12 +5,48 @@ newsgroup
 class NewsGroup
 {
 public:
-  NewsGroup();
+  NewsGroup(string n);
   ~NewsGroup();
-  addArticle();
-  removeArticle();
+  addArticle(Article a);
+  bool removeArticle(); //ASSUMES ARTICLE HAS == OPERATOR IMPLEMENT
+  sortArticles(); // ASSUMES ARTICLE HAS < OPERATOR IMPLEMENT
+  getHeadlines(); 
 private:
-  List articles; // can be vect too 
+  List<Article> articles; // can be vect too 
   string name;
   unsigned int id;
 };
+
+NewsGroup::NewsGroup(string n) name(n)
+{}
+
+NewsGroup::~NewsGroup()
+{
+	list<article>* listpointer = articles; 
+	delete listpointer; 
+}
+
+NewsGroup::addArticle(Article a)
+{
+	list.add(a); 
+}
+
+//returns true if article was removed
+
+bool NewsGroup::removeArticle(Article a)
+{
+	return articles.find(a):
+}
+
+NewsGroup::sortArticles()
+{
+	articles.sort();
+}
+
+NewsGroup::getHeadlines()
+{
+	for(article a: articles)
+	{
+		a.getTitle(); // IMPLEMENT GET TITLE
+	}
+}
